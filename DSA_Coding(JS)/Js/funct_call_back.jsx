2dc4ffc593d80f07bb3700797f6d7b0758ef2187
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-07-12 07:36:10 +0530

    Add files via upload

---
+++ DSA_Coding(JS)/Js/funct_call_back.jsx
@@ -0,0 +1,12 @@
+//WID)12/7/2026)(Sarthak Mittal(DegamieSign(function_Call_Back)))
+import React, { useCallback } from 'react'
+
+function funct_call_back() {
+    const add=useCallback(()=>{},[]);
+    const sub=useCallback(()=>{},[]);
+  return (
+    <User add>{add}</User>
+  )
+}
+
+export default funct_call_back
