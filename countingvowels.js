2cc29de6052f8d026743e1d3e2f63cbc5a79296d
Degamie_sign <degamiesign@gmail.com>
2026-08-15 09:24:23 +0530

    counting#wovels

---
+++ countingvowels.js
@@ -0,0 +1,7 @@
+//WID(15/8/2026)(Sarthak Mittal(DegamieSign(countingwovels)))
+function countvowels(str) {
+    str="Sarthak Mittal";
+    const cnt=str.match(/[aeiou] ? length || /gi);
+    console.log(cnt);
+}
+console.log(countvowels("Sarthak Mittal"));
